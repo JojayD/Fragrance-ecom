6725d1bd395bc202d4f280a64aee2ae5fac64334
JojayD <professional.josephd@gmail.com>
2024-01-06 20:52:45 -0800

    trying to commit stages

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+import pandas
+import requests
+from flask_cors import CORS
+import json
+from flask import Flask, send_from_directory, jsonify, request, make_response
+
+
+app = Flask(__name__, static_folder="frontend/  ", static_url_path="")
+CORS(app)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    if path != "" and path != "api/search":
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
+    print("API recieved")
+    df = pandas.read_csv('/Users/jojo/Desktop/Web-Dev-Projects/fragrance_ecom/backend/final_perfume_data_with_prices.csv',
+                         encoding='utf8', encoding_errors="ignore")
+    records = df.to_json(orient='records')
+
+    return jsonify(records)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
