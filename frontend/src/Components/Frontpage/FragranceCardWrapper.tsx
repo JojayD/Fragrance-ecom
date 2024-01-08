27d9d893294b0bb6ba3a5ce8b1ff5f4291e4d33c
JojayD <professional.josephd@gmail.com>
2024-01-07 21:48:08 -0800

    resolving git staged changes

---
+++ frontend/src/Components/Frontpage/FragranceCardWrapper.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import FragranceCard from "./FragranceCard";
+import styles from "/frontend/src/Styles/FragranceWrapper.module.css";
+import Header from "./Header";
+type Props = {
+	listOfFragrances: Fragrance[];
+};
+
+interface Fragrance {
+	Name: string;
+	Brand: string;
+	Description: string;
+	Notes: string;
+	ImageURL: string;
+	Price: number;
+	Id: string;
+}
+
+export default function FragranceCardWrapper({ listOfFragrances }: Props) {
+	let { id } = useParams();
+	const [fragranceData, setFragranceData] = useState<Fragrance>();
+
+	useEffect(() => {
+		const fragrance = listOfFragrances.find((f) => f.Id == id);
+		setFragranceData(fragrance);
+	}, [id, listOfFragrances]);
+
+	if (!fragranceData) {
+		return <div>Loading fragrance details...</div>;
+	}
+
+	return (
+		<>
+		<Header/>
+		<div className={styles["container__fragrance-wrapper"]}>
+			<FragranceCard
+				isDynamic={true}
+				data={fragranceData}
+			/>
+		</div>
+		</>
+	);
+}
