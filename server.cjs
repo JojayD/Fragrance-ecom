9279a5ac4a5c075adb1295d53f1827c71033aea3
JojayD <professional.josephd@gmail.com>
2024-01-15 23:23:15 -0800

    changes 6

---
+++ server.cjs
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+
+app.use(
+	"/api",
+	createProxyMiddleware({
+		target: "http://localhost:5001", // URL of your Flask backend
+		changeOrigin: true,
+	})
+);
+
+// All other requests serve the frontend index.html
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
