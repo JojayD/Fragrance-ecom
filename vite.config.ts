1852f617c366ca56dc625790682ab4b011bfae97
JojayD <professional.josephd@gmail.com>
2024-01-17 13:37:37 -0800

    removed dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server:
-		process.env.NODE_ENV === "development"
-			? {
-					proxy: {
-						"/api": "http://localhost:3000",
-					},
-			  }
-			: {},
-});
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    server:
+      process.env.NODE_ENV === "development"
+        ? {
+            proxy: {
+              "/api": "http://localhost:3000",
+            },
+          }
+        : {},
+  });
